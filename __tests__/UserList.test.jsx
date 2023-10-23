4bd7865a901b3f870a660e46af0f1131927264f4
Vishal Rahangdale <vishalrahangdale14@gmail.com>
2023-10-23 16:04:25 +0530

    final

--- __tests__/UserList.test.jsx
+++ __tests__/UserList.test.jsx
@@ -1,5 +1,7 @@
 import UserList from "@/components/UserList";
 import { render, screen } from "@testing-library/react";
+import { server } from "../mocks/server";
+import { rest } from "msw";
 
 describe("UserList - Rendering", () => {
   it("Should Have the Text vishal", async () => {
@@ -7,6 +9,16 @@ describe("UserList - Rendering", () => {
 
     expect(await screen.findByText("vishal")).toBeInTheDocument();
     expect(screen.queryByText("No. Users")).not.toBeInTheDocument();
+  });
 
+  it("Should Have Username Vishal Rahngdale", async () => {
+    server.use(
+      rest.get("/api/users", (req, res, ctx) => {
+        return res(ctx.json([{ id: 2, username: "vishal Rahngdale" }]));
+      })
+    );
+    render(<UserList />);
+
+    expect(await screen.findByText("vishal Rahngdale")).toBeInTheDocument();
   });
 });
